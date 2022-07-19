8c2a4e780cc7831f1f3c0bfc6b96bbe43cc34f5a
abhaygupta12 <71281005+abhaygupta12@users.noreply.github.com>
2022-07-19 23:48:32 +0530

    firstsave

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const app = express();
 const mongoose = require('mongoose');
-const PORT = 80;
+const PORT = process.env.PORT || 3000;
 
 const {MONGODB_URI} = require('./config');
 // require('./models/user_model');
