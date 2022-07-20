a1eb79cccacac4942a568270cad57cbf21842254
abhaygupta12 <71281005+abhaygupta12@users.noreply.github.com>
2022-07-20 10:46:37 +0530

    firstsave

--- server.js
+++ server.js
@@ -22,6 +22,14 @@ require('./models/post_model');
 
 
 
+app.use(function(req, res, next) {
+    res.header("Access-Control-Allow-Origin", '*');
+    res.header("Access-Control-Allow-Credentials", true);
+    res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
+    res.header("Access-Control-Allow-Headers", 'Origin,X-Requested-With,Content-Type,Accept,content-type,application/json');
+    next();
+});
+
 app.use(express.json());
 app.use(require('./routes/authentication'));
 app.use(require('./routes/postRoute'));
