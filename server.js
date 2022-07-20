c1b33256bc9b6f7fd32585f3e25930e824522b31
abhaygupta12 <71281005+abhaygupta12@users.noreply.github.com>
2022-07-20 11:53:42 +0530

    firstsave

--- server.js
+++ server.js
@@ -7,6 +7,13 @@ const {MONGODB_URI} = require('./config');
 // require('./models/user_model');
 // app.use(express.json());
 // app.use(require('./routes/authentication'));
+// var WebSocketServer = require('ws').Server; 
+
+// //creating a websocket server at port 9090 
+// var wss = new WebSocketServer({server: app}); 
+
+// //all connected to the server users 
+// var users = {};
 
 
 mongoose.connect(MONGODB_URI);
@@ -29,6 +36,13 @@ app.use(function(req, res, next) {
     res.header("Access-Control-Allow-Headers", 'Origin,X-Requested-With,Content-Type,Accept,content-type,application/json');
     next();
 });
+
+app.on('connection',(socket)=>{
+    console.log("a user connect");
+    socket.on("message",(ms) =>{
+        app.emit("message",ms)
+    })
+})
 
 app.use(express.json());
 app.use(require('./routes/authentication'));
