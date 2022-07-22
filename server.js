db56b69c09f039a3282fe5cee783b6f086491b44
abhaygupta12 <71281005+abhaygupta12@users.noreply.github.com>
2022-07-23 00:45:27 +0530

    thirdsave

--- server.js
+++ server.js
@@ -1,21 +1,22 @@
-const cors = require('cors');
+// const cors = require('cors');
 const express = require('express');
 const app = express();
 const mongoose = require('mongoose');
-const port = process.env.PORT || 3000;
+// const port = process.env.PORT || 3000;
+const port = 4000
 
 
 const {MONGODB_URI} = require('./config');
 // require('./models/user_model');
 // app.use(express.json());
 // app.use(require('./routes/authentication'));
-var WebSocketServer = require('ws').Server; 
+// var WebSocketServer = require('ws').Server; 
 
 // //creating a websocket server at port 9090 
-var wss = new WebSocketServer({server: app}); 
+// var wss = new WebSocketServer({server: app}); 
 
 // //all connected to the server users 
-var users = {};
+// var users = {};
 
 
 mongoose.connect(MONGODB_URI);
@@ -31,13 +32,13 @@ require('./models/post_model');
 
 
 
-app.use(function(req, res, next) {
-    res.header("Access-Control-Allow-Origin", '*');
-    res.header("Access-Control-Allow-Credentials", true);
-    res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
-    res.header("Access-Control-Allow-Headers", 'Origin,X-Requested-With,Content-Type,Accept,content-type,application/json');
-    next();
-});
+// app.use(function(req, res, next) {
+//     res.header("Access-Control-Allow-Origin", '*');
+//     res.header("Access-Control-Allow-Credentials", true);
+//     res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
+//     res.header("Access-Control-Allow-Headers", 'Origin,X-Requested-With,Content-Type,Accept,content-type,application/json');
+//     next();
+// });
 
 // app.on('connection',(socket)=>{
 //     console.log("a user connect");
@@ -50,7 +51,7 @@ app.use(express.json());
 app.use(require('./routes/authentication'));
 app.use(require('./routes/postRoute'));
 app.use(require('./routes/userRoute'));
-app.use(cors());
+// app.use(cors());
 
 app.listen(port,()=>{
     console.log("Server started ")
