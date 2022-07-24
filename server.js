3323061474bce48491a4e9c408b821d24dda72e7
Abhineet Verma <198.abhineet@gmail.com>
2022-07-24 09:20:08 +0530

    Header

--- server.js
+++ server.js
@@ -45,8 +45,9 @@ app.use(require('./routes/userRoute'));
 app.use(function(req, res, next) {
     res.header("Access-Control-Allow-Origin", '*');
     // res.header("Access-Control-Allow-Credentials", true);
-    res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
-    res.header("Access-Control-Allow-Headers", 'Origin,X-Requested-With,Content-Type,Accept,content-type,application/json');
+    // res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
+    res.header('Access-Control-Allow-Methods', '*');
+    // res.header("Access-Control-Allow-Headers", 'Origin,X-Requested-With,Content-Type,Accept,content-type,application/json');
     next();
 });
 
