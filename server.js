79a2c9b94f89176f8e48b98e5ae1c1b8318587af
Abhineet Verma <198.abhineet@gmail.com>
2022-07-24 09:02:18 +0530

    Fix server.js

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const express = require('express');
 const app = express();
 const mongoose = require('mongoose');
 // const port = process.env.PORT || 3000;
-const port = 4000
+const port = 443
 
 
 const {MONGODB_URI} = require('./config');
@@ -51,10 +51,10 @@ app.use(express.json());
 app.use(require('./routes/authentication'));
 app.use(require('./routes/postRoute'));
 app.use(require('./routes/userRoute'));
-app.use(cors());
+// app.use(cors());
 
 app.use(function(req, res, next) {
-    res.header("Access-Control-Allow-Origin", '*');
+    // res.header("Access-Control-Allow-Origin", '*');
     res.header("Access-Control-Allow-Credentials", true);
     res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
     res.header("Access-Control-Allow-Headers", 'Origin,X-Requested-With,Content-Type,Accept,content-type,application/json');
